d12dbfda8d30cb64a7e61c7a67a045ab0c215f76
RainSuds <119971665+RainSuds@users.noreply.github.com>
2023-12-22 13:44:52 -0600

    comments

--- main.cpp
+++ main.cpp
@@ -6,25 +6,35 @@ int main()
 {
 	glfwInit();
 
+	// Tell GLFW the version of OpenGL
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// create a window
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Nene Quest", NULL, NULL);
 
+	// failed to open window
 	if (window == NULL) {
 		std::cout << "Fail to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	// Introduce the window with context
 	glfwMakeContextCurrent(window);
 
+	// load GLAD
 	gladLoadGL();
 
+	// specify the viewport of OpenGL, x=0-800, y=0-800
 	glViewport(0, 0, 800, 800);
 
+	// Specify the color of the background
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color to it
 	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
 	glfwSwapBuffers(window);
 
 	while (!glfwWindowShouldClose(window))
@@ -32,7 +42,9 @@ int main()
 		glfwPollEvents();
 	}
 
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
