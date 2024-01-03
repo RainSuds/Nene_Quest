5a3ed988811c3ec6553f50c47dfe20acd70593c7
RainSuds <119971665+RainSuds@users.noreply.github.com>
2024-01-03 17:21:59 -0600

    organizing

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+
+// Vertices coordinates
+GLfloat vertices[] =
+{
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
+};
+
 int main()
 {
 	glfwInit();
@@ -12,7 +36,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// create a window
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Nene Quest", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Nene Quest", NULL, NULL);
 
 	// failed to open window
 	if (window == NULL) {
@@ -24,25 +48,36 @@ int main()
 	// Introduce the window with context
 	glfwMakeContextCurrent(window);
 
-	// load GLAD
 	gladLoadGL();
+	glViewport(0, 0, 1280, 720);
 
-	// specify the viewport of OpenGL, x=0-800, y=0-800
-	glViewport(0, 0, 800, 800);
+	Shader shaderProgram("default.vert", "default.frag");
 
-	// Specify the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// Clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swap the back buffer with the front buffer
-	glfwSwapBuffers(window);
+	VAO VAO1;
+	VAO1.Bind();
+
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkVBO(VBO1, 0);
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		shaderProgram.Activate();
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
-	// Delete window before ending the program
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
 	glfwDestroyWindow(window);
 	// Terminate GLFW before ending the program
 	glfwTerminate();
